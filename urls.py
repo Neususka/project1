8d260fd7bd42a6fb89d32c0bf1fed600dc7fb486
Neususka <61283040+Neususka@users.noreply.github.com>
2020-12-05 17:19:58 +0000

    Add files via upload
    encyclopedia

--- urls.py
+++ urls.py
@@ -1,22 +1,15 @@
-"""wiki URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('wiki/', include("encyclopedia.urls"))
+    path("", views.index, name="index"),
+    path("createnewpage", views.createnewpage, name="createnewpage"),
+    path("edit/<str:title>", views.editpage, name="editpage"),
+    path("randomentry", views.randomentry, name="randomentry"),
+    path("searchpage", views.index_search, name="index_search"),
+    path("<str:title>", views.index_geral, name="index_geral")
 ]
+
+
